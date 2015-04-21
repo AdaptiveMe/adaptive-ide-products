3ac7ea002830aaa824d0738bdb619639ac02b0af
Rafael Roman <panthro.rafael@gmail.com>
2015-04-21 17:01:22 +0200

    adding Adaptive Nibble Plugin

---
+++ common-plugins/adaptive-nibble-plugin/src/me/adaptive/ide/plugins/nibble/NibbleComponent.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2014-2015. Adaptive.me.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.adaptive.ide.plugins.nibble;
+
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.configurations.GeneralCommandLine;
+import com.intellij.execution.filters.TextConsoleBuilderFactory;
+import com.intellij.execution.process.OSProcessHandler;
+import com.intellij.execution.process.ProcessHandler;
+import com.intellij.execution.ui.ConsoleView;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.components.AbstractProjectComponent;
+import com.intellij.openapi.module.ModuleUtil;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.ui.content.ContentFactory;
+import com.intellij.ui.content.MessageView;
+import me.adaptive.ide.codewok.module.CodeWokModuleType;
+import me.adaptive.ide.codewok.npm.NpmModuleFinder;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by panthro on 17/04/15.
+ */
+public class NibbleComponent extends AbstractProjectComponent {
+
+    public static final String COMPONENT_NAME = "Adaptive Nibble";
+    public static final String NIBBLE_MODULE_NAME = "npm-adaptiveme-nibble";
+    public static final String NIBBLE_MODULE_BINARY_LOCATION = "bin" + File.separator + "adaptive-nibble-emulator" + File.separator + "bin";
+    public static final String DEFAULT_INDEX_PATH = "src" + File.separator + "index.html";
+
+    private static final String WACTHER_PARAM = "-w true";
+    //true needs to be passed https://github.com/AdaptiveMe/adaptive-tools-nibble/issues/8
+    private static final String GLOBAL_NIBBLE_COMMAND = "nibble";
+    private static final String MODULE_NIBBLE_COMMAND = "adaptive-nibble-emulator";
+
+
+    private ConsoleView consoleView;
+    private boolean withWatcher = true;
+
+    private GeneralCommandLine commandLine;
+    private volatile ProcessHandler processHandler;
+
+    protected NibbleComponent(Project project) {
+        super(project);
+    }
+
+    public void setConsoleView(ConsoleView consoleView) {
+        this.consoleView = consoleView;
+    }
+
+    private List<String> getParamList(String filePath) {
+        List<String> paramList = new LinkedList<String>();
+        paramList.add("-p");
+        paramList.add(filePath);
+        if (withWatcher) {
+            paramList.addAll(Arrays.asList(WACTHER_PARAM.split(" ")));
+        }
+        return paramList;
+    }
+
+
+    public void runOnNibble(@NotNull VirtualFile file) {
+        disposeComponent();
+
+        VirtualFile nibbleModuleRoot = NpmModuleFinder.findModuleInProject(myProject, NIBBLE_MODULE_NAME);
+        commandLine = new GeneralCommandLine();
+        if (nibbleModuleRoot != null) {
+            commandLine.setExePath(
+                    new File(nibbleModuleRoot.getPath(), NIBBLE_MODULE_BINARY_LOCATION + File.separator + MODULE_NIBBLE_COMMAND).getAbsolutePath());
+        } else {
+            commandLine.setExePath(GLOBAL_NIBBLE_COMMAND);
+        }
+        //commandLine.withWorkDirectory(myProject.getBasePath());
+        commandLine.addParameters(getParamList(file.getPath()));
+
+        try {
+            processHandler = new OSProcessHandler(commandLine);
+            if (consoleView == null) {
+                setupDefaultConsoleView();
+            } else {
+                consoleView.attachToProcess(processHandler);
+            }
+            ApplicationManager.getApplication().executeOnPooledThread(new Runnable() {
+                @Override
+                public void run() {
+                    processHandler.startNotify();
+                }
+            });
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void disposeComponent() {
+        if (isRunning()) {
+            processHandler.destroyProcess();
+            processHandler = null;
+        }
+    }
+
+    public boolean isRunning() {
+        return processHandler != null && !processHandler.isProcessTerminated() && !processHandler.isProcessTerminating();
+    }
+
+    @Override
+    public void projectClosed() {
+        disposeComponent();
+    }
+
+    @NotNull
+    @Override
+    public String getComponentName() {
+        return COMPONENT_NAME;
+    }
+
+    @Override
+    public void projectOpened() {
+        if (isAdaptiveProject()) {
+            File indexHtml = new File(myProject.getBasePath(), DEFAULT_INDEX_PATH);
+            if (indexHtml.exists() && !isRunning()) {
+                runOnNibble(myProject.getBaseDir().findFileByRelativePath(DEFAULT_INDEX_PATH));
+            }
+        }
+    }
+
+    public boolean isAdaptiveProject() {
+        return ModuleUtil.hasModulesOfType(myProject, CodeWokModuleType.getInstance());
+    }
+
+    protected void setupDefaultConsoleView() {
+        if (consoleView != null) {
+            return;
+        }
+        consoleView = TextConsoleBuilderFactory.getInstance().createBuilder(myProject).getConsole();
+        final MessageView messageView = MessageView.SERVICE.getInstance(myProject);
+        messageView.runWhenInitialized(new Runnable() {
+            @Override
+            public void run() {
+                messageView.getContentManager().addContent(ContentFactory.SERVICE.getInstance().createContent(consoleView.getComponent(), COMPONENT_NAME, false));
+                consoleView.attachToProcess(processHandler);
+            }
+        });
+    }
+}
---
+++ common-plugins/adaptive-nibble-plugin/src/me/adaptive/ide/plugins/nibble/RunOnNibbleAction.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2014-2015. Adaptive.me.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.adaptive.ide.plugins.nibble;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.Task;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+
+/**
+ * Created by panthro on 17/04/15.
+ */
+public class RunOnNibbleAction extends AnAction implements DumbAware {
+
+
+    private static final String HTML_FILE_REGEX = ".*\\.(html|htm|sht|shtm|shtml|xhtml)$";
+
+    @Override
+    public void update(final AnActionEvent e) {
+        Project project = e.getData(CommonDataKeys.PROJECT);
+        if (project == null || project.isDefault()) { //If we are not inside a project, shouldn't be able to run it
+            e.getPresentation().setVisible(false);
+            e.getPresentation().setEnabled(false);
+            return;
+        }
+        Editor editor = e.getData(CommonDataKeys.EDITOR);
+        VirtualFile file = e.getData(CommonDataKeys.VIRTUAL_FILE);
+
+        /*
+        Do not show the action in the following cases
+        Not in an editor AND no file selected OR editor is empty
+         */
+        if ((editor == null && file == null) || (editor != null && editor.getDocument().getTextLength() == 0)) {
+            e.getPresentation().setVisible(false);
+            e.getPresentation().setEnabled(false);
+            return;
+        }
+
+        /*
+        Do now show the action in case the file is not a .html file
+         */
+        if (!file.getName().matches(HTML_FILE_REGEX)) {
+            e.getPresentation().setVisible(false);
+            e.getPresentation().setEnabled(false);
+            return;
+        }
+        e.getPresentation().setVisible(true);
+        e.getPresentation().setEnabled(true);
+    }
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        final Project project = e.getData(CommonDataKeys.PROJECT);
+        if (project == null || project.isDefault()) {
+            return;
+        }
+
+        final Editor editor = e.getData(CommonDataKeys.EDITOR);
+        final VirtualFile file = e.getData(CommonDataKeys.VIRTUAL_FILE);
+        if (editor == null && file == null) {
+            return;
+        }
+
+
+        final NibbleComponent component = (NibbleComponent) project.getComponent(NibbleComponent.COMPONENT_NAME);
+        new Task.Backgroundable(project, "Launching Nibble") {
+            @Override
+            public void run(ProgressIndicator indicator) {
+                component.runOnNibble(file);
+            }
+        }.queue();
+
+
+    }
+}
