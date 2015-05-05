cd294ddd1cadc7d447aa4e00f849f831600508a0
Rafael Roman <panthro.rafael@gmail.com>
2015-05-05 13:15:54 +0200

    adding a module to hold all icons used in the common plugins

---
+++ common-plugins/common-icons/src/me/adaptive/ide/icons/AdaptiveIcons.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.adaptive.ide.icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * Created by panthro on 5/05/15.
+ */
+public class AdaptiveIcons {
+
+  private static Icon load(String path){
+    return IconLoader.getIcon(path, AdaptiveIcons.class);
+  }
+
+  public static final Icon Adaptive =  load("/icons/adaptive.png");
+  public static final Icon BowerIcon = load("/icons/bower.png");
+  public static final Icon NodeJs =  load("/icons/nodejs.png");
+  public static final Icon Grunt =  load("/icons/grunt.png");
+}
